e26d761f09d540f9e710377fd38a8497cc0fe886
apina <helvetinperse666@gmail.com>
2017-03-01 20:13:14 +0200

    displays real time data in the TV

--- app/src/main/java/com/example/temp_monitor/MainActivity.java
+++ app/src/main/java/com/example/temp_monitor/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.temp_monitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final String TAG = "MainActivity";
+    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("Leppavaara/newest/humidity");
+    TextView tvhumidity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvhumidity = (TextView) findViewById(R.id.tvhumidity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Read from the database
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                tvhumidity.setText(value.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
